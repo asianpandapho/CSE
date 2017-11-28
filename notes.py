33d3ee783a1b3c6baa55977872d89b5c07a86b61
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2017-11-28 15:07:15 -0800

    Adding Previous Notes
    11/27/17

---
+++ notes.py
@@ -0,0 +1,37 @@
+print ("Hello World")
+
+# print ("Anou Is Asian Daddy")
+
+print (3 + 5)
+print (5 - 3)
+print (5 * 3)
+print (6 / 2)
+print (3 ** 2)
+print () # Create a blank line
+print ("See if you can figure this out")
+print (13 % 12)
+print (15 % 5)
+print (5 % 3)
+print (14 % 14)
+print (-12 % 2)
+print (1 % 1)
+print (1004 % 13)
+
+# Variable
+car_name = "Anou Mobile for you to DRIVE"
+car_type = "Lamborghini Aventador Sesto Elemento Honda Subaru"
+car_cylinders = 8
+car_mpg =  9000.1
+
+# Inline Printing
+print ("My car is a Honda Civic %s " % car_name)
+print ("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking Input
+name = input ("What is your name?")
+print ("Hello %s" % name)
+print(name)
+# print (name)
+age = input ("What is your age?")
+print ("You are %s" % age)
+print(age)
