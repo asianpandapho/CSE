d6daa18e079ecd4ae77fe9f16d0c00a91f383b86
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2017-12-08 15:42:26 -0800

    Lucky7
    new game

---
+++ Lucky7's Anou Her.py
@@ -0,0 +1,14 @@
+import random
+
+
+dice1= (random.randint(1, 6))
+dice2 = (random.randint(1, 6))
+rigged_stuff = 7
+money = 15
+
+
+def random_number(rigged):
+    roll = (dice1 + dice2)
+
+
+# Anou Her
