6702dd90099ec11708e2375c7fc4aa132dcf787d
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-01-10 15:41:19 -0800

    money is trouble
    cant do end

--- Lucky7's Anou Her.py
+++ Lucky7's Anou Her.py
@@ -1,23 +1,28 @@
 import random
 
 
-rigged_stuff = 7
 money = 15
-
+played = 0
 
 while money > 0:
-    dice1 = (random.randint(1, 6))
-    dice2 = (random.randint(1, 6))
-    print(dice1)
-    print(dice2)
+    dice1 = (random.randint(0, 6))
+    dice2 = (random.randint(0, 6))
+    print("dice 1 : %s" % dice1)
+    print("dice 2 : %s" % dice2)
+    played += 1
 
     roll = (dice1 + dice2)
 
-    if roll == rigged_stuff:
-        print(money + 4)
+    if roll == 7:
+        money += 4
     elif roll != 7:
-        print(money - 1)
+        money -= 1
+        print(money)
 
+if money == 0:
+    print("You've played %s rounds!" % str(played))
 
+if money > 15:
+    print("The most money you've had playing this game is %s!" % money)
 
 # Anou Her
