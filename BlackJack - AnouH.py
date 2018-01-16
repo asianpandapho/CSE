b5c94b10523c79d18b876aeb50720795872c578f
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-01-16 10:47:41 -0800

    o
    o

--- BlackJack - AnouH.py
+++ BlackJack - AnouH.py
@@ -1,6 +1,7 @@
 import random
 
-random.randint(1, 10)
+random.randint(1, 11)
+
 King = 10
 Queen = 10
 Jack = 10
@@ -12,3 +13,10 @@ Seven = 7
 Six = 6
 Five = 5
 Four = 4
+Three = 3
+Two = 2
+One = 1
+
+
+
+# Anou Her
