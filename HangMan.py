ed9fef899cdb1a592e732778feafd6414e05ad2f
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-01-23 15:40:26 -0800

    yeet
    nancy is negative

--- HangMan.py
+++ HangMan.py
@@ -5,7 +5,7 @@ import string
 A general guide for Hangman
 1.Make a word bank - 10
 2.Pick a random item from the list
-3.Add a guess to the list on letters guessed
+3.Add a user input to the list on letters guessed
 4.Reveal the letters that have been guessed
 5.Create the win condition
 
@@ -16,32 +16,37 @@ list2 = list(string.ascii_uppercase)
 word = (random.choice(list1))
 print(word)
 print("Lets play Hangman, you have 10 guesses, what am I thinking?")
-already_guessed = [" ", ]
-right_guesses = [" ", ]
+already_guessed = []
+right_guesses = []
 
 while guesses > 0:
     guesses -= 1
     print(guesses)
     print("These are your letters, %s" % list2)
-    end = input("Do you want to guess?")
-    answer = input("What do you think it is")
     ask_for_letter = input("Name a letter")
-    print(end)
-    if end == "no":
-        print("Ok")
+    high = ask_for_letter.upper()
+    already_guessed.append(high)
+    print(already_guessed)
 
-        if end == "Yes":
-            print(answer)
-        if answer == word:
+    if ask_for_letter == list2 in word:
+        right_guesses.append(list2)
+
+
+
+    # if end == "no":
+    #     print("Ok")
+    #
+    #     if end == "Yes":
+    #         print(answer)
+        if right_guesses == word:
             print(word)
             print("You Win! XD")
 
-    if str(list2) in word is True:
-        guesses + 1
-
-        if list2 != string.ascii_uppercase:
-            list2 = string.ascii_uppercase
-
-
+    # if str(list2) in word is True:
+    #     guesses + 1
+    #
+    #     if already_guessed != string.ascii_uppercase:
+    #          = string.ascii_uppercase
+    #
 
 # Anou Her
