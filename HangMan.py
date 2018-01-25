1892c2f79a16d588a079257e15b5cfa281347582
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-01-25 15:41:33 -0800

    v
    XD

--- HangMan.py
+++ HangMan.py
@@ -10,42 +10,35 @@ A general guide for Hangman
 5.Create the win condition
 
 """
-word_bank = ["YASUO", "RENGAR", "KATARINA", "GRAVES", "ZED", "AKALI", "ZOE", "FIZZ", "FIORA", "HASAGI"]
+word_bank = ["YASUO", "RENGAR", "KATARINA", "GRAVES", "ZED", "AKALI", "UDYR", "FIZZ", "FIORA", "HASAGI"]
 guesses = 10
 alphabet = list(string.ascii_uppercase)
 random_word = random.choice(word_bank)
+correct = list(random_word)
 print(random_word)
 print("Lets play Hangman, you have 10 guesses, what am I thinking?")
 letters_guessed = []
 
+
 while guesses > 0:
+    output = []
+    for letter in random_word:
+        if letter in letters_guessed:
+            output.append(letter)
+        else:
+            output.append("*")
+    print(output)
     guesses -= 1
-    print(guesses)
+    if output == correct:
+        print("You Win")
+        exit(0)
     print("These are your letters, %s" % alphabet)
     ask_for_letter = input("Name a letter")
     uppercase_guess = ask_for_letter.upper()
     letters_guessed.append(uppercase_guess)
     if uppercase_guess in alphabet:
         alphabet.remove(uppercase_guess)
-    for letter in random_word:
-        if uppercase_guess in random_word:
-            letters_guessed.append(uppercase_guess)
-            print(letters_guessed)
-    if str(alphabet) in random_word is True:
-        guesses + 1
-
-
-    # if righ_guesses == word:
-    #     print("YOU WIN XD")
-    #
-    #     if end == "Yes":
-    #         print(answer)
-    if letters_guessed == random_word:
-        print(random_word)
-        print("You Win XD!")
-
-    #     if already_guessed != string.ascii_uppercase:
-    #          = string.ascii_uppercase
-    #
+    if guesses == 0:
+        print("You Lose the word was %s" % random_word)
 
 # Anou Her
