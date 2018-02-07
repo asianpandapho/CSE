12f5bbed44243569db0c95322142b004ea1d17d6
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-02-07 10:48:06 -0800

    starting
    with project

---
+++ Jurrasic Park Map- Anou Her.py
@@ -0,0 +1,23 @@
+world_map = {
+    'AIRPLANE': {
+        'NAME': 'AIRPLANE LENDING AREA',
+        'DESCRIPTION': 'You are in the airplane landing area, you can only go north to go to the gate.',
+        'PATHS': {
+            'N': 'NORTH'
+        }
+    },
+    'GATE': {
+        'NAME': 'GATE ENTRANCE',
+        'DESCRIPTION': 'You at the entrance of the park, you can go to the store by going W or the Visitor center to'
+                       'the East. You could also go ',
+        'PATHS': {
+            'NORTH': 'NORTHHOUSE'
+        }
+    }
+}
+
+
+
+
+
+# Anou Her
