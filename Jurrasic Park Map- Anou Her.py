2bac3ad55f7b92d0008706a0904d7277aa68e0b6
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-02-13 10:48:11 -0800

    happy
    hi man

--- Jurrasic Park Map- Anou Her.py
+++ Jurrasic Park Map- Anou Her.py
@@ -8,13 +8,89 @@ world_map = {
     },
     'GATE': {
         'NAME': 'GATE ENTRANCE',
-        'DESCRIPTION': 'You at the entrance of the park, you can go to the store by going W or the Visitor center to'
-                       'the East. You could also go ',
+        'DESCRIPTION': 'You are at the entrance of the park there are paths East, West, Northeast, and Northwest',
         'PATHS': {
-            'NORTH': 'NORTHHOUSE'
+            'NE': 'NORTHEAST',
+            'E': 'EAST',
+            'W': 'WEST',
+            'NW': 'NORTHWEST'
         }
-    }
+    },
+    'LAB': {
+        'NAME': 'LABORATORY',
+        'DESCRIPTION': 'You go to the Laboratory and look at how the genetically modify dinosaurs,'
+                       ' conveniently there is a button with the marking DANGER! on it, there is a path Northeast',
+        'PATHS': {
+            'NE': 'NORTHEAST',
+        }
+    },
+    'VISIT': {
+        'NAME': 'VISITOR CENTER',
+        'DESCRIPTION': 'You are at the Visitor Center here visitors can buy items, there is a path Northwest,',
+        'PATHS': {
+            'NW': 'NORTHWEST',
+        }
+    },
+    'VELO': {
+        'NAME': 'VELOCIRAPTOR CAGE',
+        'DESCRIPTION': 'You arrive at the Velociraptor Cage where you see 4 Velociraptors running around and jumping.'
+                       ' There are paths Northeast, North, and West',
+        'PATHS': {
+            'NW': 'NORTHEAST',
+            'N': 'NORTH',
+            'W': 'WEST'
+        }
+    },
+    'TRI': {
+        'NAME': 'TRICERATOPS CAGE',
+        'DESCRIPTION': 'You arrive at the Triceratops Cage, there is a Triceratops munching on some leaves,'
+                       ' there are paths Northwest, East, and North',
+        'PATHS': {
+            'NW': 'NORTHWEST',
+            'N': 'NORTH',
+            'E': 'EAST'
+        }
+    },
+    'BRACHIO': {
+        'NAME': 'BRACHIOSAURUS CAGE',
+        'DESCRIPTION': 'You arrive at the Brachiosaurus Cage, you see huge dinosaurs walking around and eating from'
+                       ' massive trees, there are paths West, East, and North',
+        'PATHS': {
+            'W': 'WEST',
+            'N': 'NORTH',
+            'E': 'EAST'
+        }
+    },
+    'SPINO': {
+        'NAME': 'SPINOSAURUS CAGE',
+        'DESCRIPTION': 'You arrive at the Spinosaurus Cage and see a huge Spinosaurus, it look very hungry,'
+                       ' there are paths East, North, and West',
+        'PATHS': {
+            'W': 'WEST',
+            'N': 'NORTH',
+            'E': 'EAST'
+        }
+    },
+    'DILOPH': {
+        'NAME': 'DILOPHOSAURUS CAGE',
+        'DESCRIPTION': 'You arrive at the Dilophosaurus Cage and see 4 Dilophosaurus fighting, '
+                       'and they look very hungry, there are paths West and Northwest',
+        'PATHS': {
+            'W': 'WEST',
+            'NW': 'NORTHWEST'
+        }
+    },
+    'MEGA': {
+        'NAME': 'MEGALODON CAGE',
+        'DESCRIPTION': 'You arrive at the Dilophosaurus Cage and see 4 Dilophosaurus fighting, '
+                       'and they look very hungry, there are paths West and Northwest',
+        'PATHS': {
+            'W': 'WEST',
+            'NW': 'NORTHWEST'
+        }
+    },
 }
+
 
 
 
