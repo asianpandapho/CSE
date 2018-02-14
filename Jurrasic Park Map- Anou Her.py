ca97bb0faca1d9e1c834d6c40648babf35a1aa79
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-02-14 10:49:04 -0800

    SO MUCH DONE
    -_-

--- Jurrasic Park Map- Anou Her.py
+++ Jurrasic Park Map- Anou Her.py
@@ -1,7 +1,8 @@
 world_map = {
     'AIRPLANE': {
         'NAME': 'AIRPLANE LENDING AREA',
-        'DESCRIPTION': 'You are in the airplane landing area, you can only go north to go to the gate.',
+        'DESCRIPTION': 'You are in the airplane landing area, you can only go north to go to the gate.\n'
+                       'Use N,S,W,E, NE, NW, SE, SW to move.',
         'PATHS': {
             'N': 'GATE'
         }
@@ -11,12 +12,13 @@ world_map = {
         'DESCRIPTION': 'You are at the entrance of the park, '
                        'You are a electrician and you came to do a checkup\n'
                        'You have on you a screwdriver and a flashlight\n'
-                       'there are paths East, West, Northeast, and Northwest',
+                       'there are paths East, West, Northeast, and Northwest, and South',
         'PATHS': {
             'NE': 'VELO',
             'E': 'VISIT',
             'W': 'LAB',
-            'NW': 'TRI'
+            'NW': 'TRI',
+            'S': 'AIRPLANE'
         }
     },
     'LAB': {
@@ -24,14 +26,14 @@ world_map = {
         'DESCRIPTION': 'You go to the Laboratory and look at how the genetically modify dinosaurs\n'
                        ' conveniently there is a button with the marking DANGER! on it, there is a path Northeast',
         'PATHS': {
-            'NE': 'NORTHEAST',
+            'NE': 'TRI',
         }
     },
     'VISIT': {
         'NAME': 'VISITOR CENTER',
         'DESCRIPTION': 'You are at the Visitor Center here visitors can buy items, there is a path Northwest,',
         'PATHS': {
-            'NW': 'NORTHWEST',
+            'NW': 'VELO',
         }
     },
     'VELO': {
@@ -39,9 +41,9 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Velociraptor Cage where you see 4 Velociraptors running around and jumping.\n'
                        ' There are paths Northeast, North, and West',
         'PATHS': {
-            'NW': 'NORTHEAST',
-            'N': 'NORTH',
-            'W': 'WEST'
+            'NE': 'DILOPH',
+            'N': 'SPINO',
+            'W': 'TRI'
         }
     },
     'TRI': {
@@ -49,9 +51,9 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Triceratops Cage, there is a Triceratops munching on some leaves,\n'
                        ' there are paths Northwest, East, and North',
         'PATHS': {
-            'NW': 'NORTHWEST',
-            'N': 'NORTH',
-            'E': 'EAST'
+            'NW': 'MEGA',
+            'N': 'BRACHIO',
+            'E': 'VELO'
         }
     },
     'BRACHIO': {
@@ -59,9 +61,9 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Brachiosaurus Cage, you see huge dinosaurs walking around and eating from\n'
                        ' massive trees, there are paths West, East, and North',
         'PATHS': {
-            'W': 'WEST',
-            'N': 'NORTH',
-            'E': 'EAST'
+            'W': 'MEGA',
+            'N': 'STEGO',
+            'E': 'SPINO'
         }
     },
     'SPINO': {
@@ -69,9 +71,9 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Spinosaurus Cage and see a huge Spinosaurus, it look very hungry,\n'
                        ' there are paths East, North, and West',
         'PATHS': {
-            'W': 'WEST',
-            'N': 'NORTH',
-            'E': 'EAST'
+            'W': 'BRACHIO',
+            'N': 'T REX',
+            'E': 'DILOPH'
         }
     },
     'DILOPH': {
@@ -79,17 +81,17 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Dilophosaurus Cage and see 4 Dilophosaurus fighting,\n'
                        'and they look very hungry, there are paths West and Northwest',
         'PATHS': {
-            'W': 'WEST',
-            'NW': 'NORTHWEST'
+            'W': 'SPINO',
+            'NW': 'T REX'
         }
     },
     'MEGA': {
         'NAME': 'MEGALODON CAGE',
         'DESCRIPTION': 'You arrive at the Dilophosaurus Cage and see 4 Dilophosaurus fighting,\n'
-                       'and they look very hungry, there are paths West and Northwest',
+                       'and they look very hungry, there are paths East and Northeast',
         'PATHS': {
-            'W': 'WEST',
-            'NW': 'NORTHWEST'
+            'E': 'BRACHIO',
+            'NE': 'STEGO'
         }
     },
     'T REX': {
@@ -97,8 +99,8 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Tyrannosaurus Rex Cage, there you see the king of dinosaurs,\n'
                        ' there are paths North and West',
         'PATHS': {
-            'W': 'WEST',
-            'N': 'NORTH'
+            'W': 'STEGO',
+            'N': 'I REX'
         }
     },
     'STEGO': {
@@ -106,8 +108,8 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Stegosaurus Cage, you see the stegosaurus drinking water,\n'
                        ' there are paths North and East',
         'PATHS': {
-            'E': 'EAST',
-            'N': 'NORTH'
+            'E': 'T REX',
+            'N': ''
         }
     },
     'I REX': {
@@ -115,8 +117,17 @@ world_map = {
         'DESCRIPTION': 'You arrive at the Indominous Rex Cage, you cant see anything but trees,\n'
                        ' however you feel a presence, there are paths Northwest and West',
         'PATHS': {
-            'W': 'WEST',
-            'NW': 'NORTHWEST'
+            'W': 'PTER',
+            'NW': 'COPTER'
+        }
+    },
+    'PTER': {
+        'NAME': 'PTERODACTYL CAGE',
+        'DESCRIPTION': 'You arrive at the Pterodactyl Cage, you see them fly in the sky,\n'
+                       ' there are paths Northeast and East',
+        'PATHS': {
+            'E': 'I REX',
+            'NE': 'COPTER'
         }
     },
     'COPTER': {
@@ -125,10 +136,22 @@ world_map = {
     }
 }
 
-while
-    current_node = world_map["AIRPLANE"]
+current_node = world_map["AIRPLANE"]
+directions = ['N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW']
+
+while True:
     print(current_node["NAME"])
     print(current_node["DESCRIPTION"])
-
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print('You cannot go this way')
+    else:
+        print('command not Recognized')
 
 # Anou Her
