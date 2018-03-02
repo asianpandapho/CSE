19f14ea1b01c58f0930b7b8a8067f00d9b06e9b0
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-03-02 10:40:28 -0800

    oof
    ok

--- Class Notes.py
+++ Class Notes.py
@@ -1,34 +1,34 @@
 # Defining a class
-class Shoes(object):
-    def __init__(self, lace_color, lighting, brand):  # TWO underscores before and after
-        # Things a shoe has
-        self.lace_color = lace_color
-        self.rgb_lighting = lighting
-        self.used = False
-        self.brand = brand
-        self.clean = True
-
-    def wear(self):
-        self.used = True
-        self.clean = False
-        print('You wear the shoes')
-
-    def wash(self):
-        self.clean = True
-        print("You cleaned the shoes")
-
-
-first_pair = Shoes("Red", "True", "Sketchers")
-second_pair = Shoes("Blue", "True", "Reebok")
-
-print(first_pair.brand)
-print(second_pair.lace_color)
-print(first_pair.clean)
-
-first_pair.wear()
-print(first_pair.clean)
-first_pair.wash()
-print(first_pair.clean)
+# class Shoes(object):
+#     def __init__(self, lace_color, lighting, brand):  # TWO underscores before and after
+#         # Things a shoe has
+#         self.lace_color = lace_color
+#         self.rgb_lighting = lighting
+#         self.used = False
+#         self.brand = brand
+#         self.clean = True
+#
+#     def wear(self):
+#         self.used = True
+#         self.clean = False
+#         print('You wear the shoes')
+#
+#     def wash(self):
+#         self.clean = True
+#         print("You cleaned the shoes")
+#
+#
+# first_pair = Shoes("Red", "True", "Sketchers")
+# second_pair = Shoes("Blue", "True", "Reebok")
+#
+# print(first_pair.brand)
+# print(second_pair.lace_color)
+# print(first_pair.clean)
+#
+# first_pair.wear()
+# print(first_pair.clean)
+# first_pair.wash()
+# print(first_pair.clean)
 
 
 class Car(object):
@@ -41,6 +41,7 @@ class Car(object):
         self.model = model
         self.horsepower = horsepower
         self.running = running
+    # Hai
 
     def drive(self):
         self.used = True
@@ -63,3 +64,17 @@ class Car(object):
         else:
             self.running = True
             print("You started the car")
+
+    def turn_off(self):
+        if self.running:
+            print("Nothing happens")
+        else:
+            self.running = False
+            print("Your car is not on")
+
+
+mycoolcar = Car(4, "Blue", "Gold" "Lambo", "100", "True", "True")
+mycoolcar.turn_on()
+mycoolcar.drive_forward()
+mycoolcar.turn_off()
+
