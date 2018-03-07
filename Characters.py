760f2fa2454bcee3bca2edd4837668eafcdcb6e9
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-03-07 15:39:41 -0800

    oh man
    KAI'SAI IS OUT BOYYZZZ

--- Characters.py
+++ Characters.py
@@ -1,37 +1,51 @@
+import random
+
+
 class Characters(object):
-    def __init__(self, name, description, health, attack):
+    def __init__(self, name, description, health, attack, block):
         self.name = name
         self.description = description
         self.dead = False
         self.health = health
         self.attack = attack
         self.enemy = False
+        self.block = block
 
     def take_damage(self, amt):
         self.health -= amt
-        self.dead = True
-        print("You knocked out")
 
     def fight(self, enemy):
         print('You engage in a fight with the %s' % bad.name)
-        enemy.take_damage(self.attack)
+        choice = random.choice([enemy, self])
+        if choice == self:
+            self.attack = True
+            enemy.take_damage(self.attack)
+            print('You attack the %s' % enemy.name)
+        elif choice == enemy:
+            self.take_damage(enemy.attack)
+            enemy.attack = True
         if self.attack >= enemy.health:
             enemy.dead = True
+            print('%s knocked out' % enemy.name)
         if enemy.attack >= self.health:
             self.dead = True
 
+    def defend(self, enemy):
+        choice = random.choice([enemy, self])
+        if choice == self:
+            enemy.attack = False
+            print('You blocked the attack')
+        if choice == enemy:
+            enemy.attack = True
 
-you = Characters('you', 'you are yourself', 45, 10)
-bad = Characters('dino', 'This is a bad guy', 10, 1)
 
-print(you.name)
-print(you.description)
-print(you.health)
+you = Characters('you', 'you are yourself', 3, 1, 1)
+bad = Characters('dino', 'This is a bad guy', 1, 4, 1)
+
+print(you.defend(bad))
 print(you.attack)
 
-print(bad.name)
-print(bad.description)
-print(bad.health)
+
 print(bad.attack)
 
 you.fight(bad)
