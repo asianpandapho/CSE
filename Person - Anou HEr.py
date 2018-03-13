352529933ac58834bd97aa2157f0ea2168067e9b
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-03-13 15:40:21 -0700

    ytf
    fkbj

--- Person - Anou HEr.py
+++ Person - Anou HEr.py
@@ -4,22 +4,32 @@ class Person(object):
         self.age = age
 
     def work(self):
-        print("%s goes to work" % self.name)
+        print("%s goes to work." % self.name)
 
 
 class Employee(Person):
-    def __init__(self, name, age):
+    def __init__(self, name, age, job):
         super(Employee, self).__init__(name, age)
         self.job = job
 
-
-class Programmer(Person):
-    def __init__(self, name, age):
-        super(Programmer, self).__init__(name, age)
+    def job(self):
+        print("He makes %s" % self.job)
 
 
-steve = Employee("Steve", 29)
+class Programmer(Employee):
+    def __init__(self, name, age, job, computer):
+        super(Programmer, self).__init__(name, age, job)
+        self.computer = computer
+
+    def comp(self):
+        print('He uses an %s' % self.computer)
+
+
+steve = Employee("Steve", 29, 'Food')
 Employee.work(steve)
+Employee.job(steve)
 
-Weibe_Chan = Programmer("Weibe-Sama", 900)
+Weibe_Chan = Programmer("Weibe-Sama", 900, 'Games', 'Alienware Aurora')
 Programmer.work(Weibe_Chan)
+Programmer.job(Weibe_Chan)
+Programmer.comp(Weibe_Chan)
