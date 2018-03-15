204ca13ac55ea32ec264bf6186dc0b4d07114d5b
asianpandapho <33767537+asianpandapho@users.noreply.github.com>
2018-03-15 10:47:04 -0700

    wow
    cool

--- Game Items.py
+++ Game Items.py
@@ -1,12 +1,33 @@
 
 class Characters(object):
-    def __init__(self, name, description, health, attack):
+    def __init__(self, name, desc):
         self.name = name
-        self.description = description
+        self.desc = desc
         self.dead = False
+
+
+class Enemy(Characters):
+    def __init__(self, name, desc, health, damage):
+        super(Enemy, self).__init__(name, desc)
         self.health = health
-        self.attack = attack
-        self.enemy = False
+        self.damage = damage
+
+
+class Carnivore(Enemy):
+    def __init__(self, name, desc, big, small):
+        super(Carnivore, self).__init__(name, desc)
+        self.big = big
+        self.small = small
+
+
+class TRex(Carnivore):
+    def __init__(self):
+        super(TRex, self).__init__("T Rex", "A T Rex that does tons of damage", True, False)
+
+
+class IRex(Carnivore):
+    def __init__(self):
+        super(IRex, self).__init__('I Rex', "An I Rex that does tons of damage", True, False)
 
 
 class Item(object):
@@ -71,3 +92,24 @@ class Lighter(Misc):
     def __init__(self, light):
         super(Lighter, self).__init__("Lighter", "A small lighter", 1)
         self.light = light
+
+
+class Net(Misc):
+    def __init__(self):
+        super(Net, self).__init__("Net", "A big net", 1)
+
+
+class Healing(Item):
+    def __init__(self, name, desc, uses):
+        super(Healing, self).__init__(name, desc)
+        self.uses = uses
+
+
+class MedKit(Healing):
+    def __init__(self):
+        super(MedKit, self).__init__("Med Kit", "A Med Kit that will fully heal you", 1)
+
+
+class Bandage(Healing):
+    def __init__(self):
+        super(Bandage, self).__init__('Bandage', "A Bandage that will heal you for 10 health", 1)
